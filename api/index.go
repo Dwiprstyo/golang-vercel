91c14df028cc0bdab68ad9fb78d0b616e22332d1
Dwi Prasetyo <prasetyo@cnt.id>
2025-01-30 15:00:35 +0700

    coba kasih port

--- api/index.go
+++ api/index.go
@@ -1,10 +1,23 @@
-package handler
+package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+// Handler function from your original code
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go AAAAA!</h1>")
+}
+
+func main() {
+	// Register the handler function for all paths
+	http.HandleFunc("/", handler)
+
+	// Start the server on port 3000
+	fmt.Println("Server starting on http://localhost:3000")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
